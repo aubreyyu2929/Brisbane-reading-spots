6cc09864ac71112db3f93c1be6f6a1a8330b1f1b
tianbo-UQ <yzztb2@gmail.com>
2019-10-04 10:22:56 +1000

    added comment and login page

--- foryou.php
+++ foryou.php
@@ -15,9 +15,43 @@ include("header.php"); ?>
 		</div>
 		<p>Distance within: <span id="sliderDist"></span> Km</p>
 		<h1>Records from SLQ</h1>
-		<section id="records"></section>
+		<section id="records">
+			<div class="setgrade">  
+				<div class="grade">
+					<span id="getgrade">0.0</span>
+				</div>
+    			<div class="star">
+        			<ul id="star">
+						<li>
+							<a href="javascript:void(0)" onclick="halfStar(1)"></a>
+							<a href="javascript:void(0)" onclick="halfStar(2)"></a>
+						</li>
+						<li>
+							<a href="javascript:void(0)" onclick="halfStar(3)"></a>
+							<a href="javascript:void(0)" onclick="halfStar(4)"></a>
+						</li>
+						<li>
+							<a href="javascript:void(0)" onclick="halfStar(5)"></a>
+							<a href="javascript:void(0)" onclick="halfStar(6)"></a>
+						</li>
+						<li>
+							<a href="javascript:void(0)" onclick="halfStar(7)"></a>
+							<a href="javascript:void(0)" onclick="halfStar(8)"></a>
+						</li>
+						<li>
+							<a href="javascript:void(0)" onclick="halfStar(9)"></a>
+							<a href="javascript:void(0)" onclick="halfStar(10)"></a>
+						</li>
+        			</ul>
+    			</div>
+    			<div class="sure">
+					<button id="sure">confirm</button>
+					<button id="cancel">cancel</button>
+    			</div>
+			</div>
+		</section>
 		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 		<script src="script.js"></script>
-			<?php include("footer.php"); ?>
+		<?php include("footer.php"); ?>
 	
 		
